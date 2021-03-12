e9b1d7e1f6dfdfb5b72653e713f9e445ecd7ff65
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-03-12 17:42:33 -0500

    Parallax background setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+import starsBG from "./background.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Parallax bgImage={starsBG} bgImageAlt="starsBG" strength={-200}>
+        {" "}
+        <div style={{ position: "absolute", height: "500px" }} />
+        <p>Hello World</p>
+      </Parallax>
     </div>
   );
 }
