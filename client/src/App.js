6adc4ee7175a9bdd20635950c0d02e624adcc20b
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-03-13 11:11:44 -0500

    installed Parralax scroll npm

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Parallax } from "react-parallax";
+import { ParallaxProvider } from "react-scroll-parallax";
 import starsBG from "./background.jpg";
 import Navbar from "../src/containers/navbar/navbar.js";
 import AboutMe from "../src/containers/aboutMe/aboutMe";
@@ -8,17 +9,19 @@ import Footer from "../src/containers/footer/footer";
 function App() {
   return (
     <div className="App">
-      <Parallax
-        bgImage={starsBG}
-        bgImageAlt="starsBG"
-        strength={-200}
-        style={{ height: "700px" }}
-      >
-        {" "}
-        <Navbar />
-        <AboutMe />
-        <Footer />
-      </Parallax>
+      <ParallaxProvider>
+        <Parallax
+          bgImage={starsBG}
+          bgImageAlt="starsBG"
+          strength={-200}
+          style={{ height: "700px" }}
+        >
+          {" "}
+          <Navbar />
+          <AboutMe />
+          <Footer />
+        </Parallax>
+      </ParallaxProvider>
     </div>
   );
 }
