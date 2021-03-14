8d6e48fcfbfb70dcc82ebe7b70c2edd4cd6db07d
kenfcheng <64857091+kenfcheng@users.noreply.github.com>
2021-03-14 15:18:33 -0400

    installed  styled-component npm and created styled component folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import React from "react";
-import { Parallax } from "react-parallax";
-import { ParallaxProvider } from "react-scroll-parallax";
-import starsBG from "./background.jpg";
-import Navbar from "../src/containers/navbar/navbar.js";
-import AboutMe from "../src/containers/aboutMe/aboutMe";
-import Footer from "../src/containers/footer/footer";
+import { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <ParallaxProvider>
-        <Parallax
-          bgImage={starsBG}
-          bgImageAlt="starsBG"
-          strength={-200}
-          style={{ height: "700px" }}
-        >
-          {" "}
-          <Navbar />
-          <AboutMe />
-          <Footer />
-        </Parallax>
-      </ParallaxProvider>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const App = () => (
+  <>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
---
+++ client/src/components/styled/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Parallax = styled.div`
+  background: url("../background.jpg") center;
+  background-size: cover;
+  background-attachment: fixed;
+  width: 100vw;
+`;
